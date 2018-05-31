d6982f444305d133d17ab78523cb2b7a975685c9
zzq <1521154719@qq.com>
2018-05-31 15:07:30 +0800

    Initial Commit

---
+++ 渐变色/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  渐变色
+//
+//  Created by zzq on 2018/5/31.
+//  Copyright © 2018年 zzq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
